b6a8c6feb943cd59de19deaff989147768e3c711
michel <michel.kuhn@live.com>
2017-05-21 13:25:32 +0200

    Karten können gespielt werden

--- app/src/main/java/com/example/michel/trinkelberg/Card.java
+++ app/src/main/java/com/example/michel/trinkelberg/Card.java
@@ -2,22 +2,20 @@ package com.example.michel.trinkelberg;
 
 import java.util.ArrayList;
 
-/**
- * Created by michel on 21.05.17.
- */
-
 public class Card {
     private String text;
     private int sips;
     private boolean glass;
     private boolean virus;
+    private boolean cure;
     private ArrayList<String> attributes;
 
-    public Card(String text, int sips, boolean glass, boolean virus, ArrayList<String> attributes) {
+    public Card(String text, int sips, boolean glass, boolean virus, boolean cure, ArrayList<String> attributes) {
         this.text = text;
         this.sips = sips;
         this.glass = glass;
         this.virus = virus;
+        this.cure = cure;
         this.attributes = attributes;
     }
 
@@ -28,5 +26,17 @@ public class Card {
             }
         }
         return true;
+    }
+
+    public String playCard(Player player) {
+        player.addSips(this.sips);
+        if(this.glass) { player.addGlass(); }
+        if(this.virus) {
+            player.infect();
+        } else if (this.cure) {
+            player.cure();
+        }
+
+        return this.text + player.getName();
     }
 }
