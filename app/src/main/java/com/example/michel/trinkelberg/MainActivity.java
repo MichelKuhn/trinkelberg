55eecb3f86aea58e8c9ced9aecf83a99fe308211
michel <michel.kuhn@live.com>
2017-05-23 00:54:55 +0200

    Late Night Coding

--- app/src/main/java/com/example/michel/trinkelberg/MainActivity.java
+++ app/src/main/java/com/example/michel/trinkelberg/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.michel.trinkelberg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    ArrayAdapter<String> adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        ListView lv = (ListView)findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+    }
+
+    public void addItems(View v) {
+        TextView tv = (TextView)findViewById(R.id.editText);
+        listItems.add(tv.getText().toString());
+        adapter.notifyDataSetChanged();
+        tv.setText("");
     }
 }
